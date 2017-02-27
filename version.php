accbda4b73386d81fcdc45fb90d61dff30c9f6e4
Hittesh Ahuja <ha386@bath.ac.uk>
2017-02-27 11:55:02 +0000

    Added version file

--- version.php
+++ version.php
@@ -5,3 +5,10 @@
  * Date: 09/02/2017
  * Time: 11:23
  */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2015111600;        // The current module version (YYYYMMDDXX)
+$plugin->requires  = 2015111000;        // Requires this Moodle version.
+$plugin->component = 'local_bath_grades_transfer';
+$plugin->cron      = 60;                // Give as a chance every minute.
