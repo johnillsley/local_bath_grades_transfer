3d89daf485e2cb93e538b165b00372f360f75de3
Hittesh Ahuja <ha386@bath.ac.uk>
2017-03-01 14:57:28 +0000

    Added strings

--- lan/en/local_bath_grades_transfer.php
+++ lan/en/local_bath_grades_transfer.php
@@ -1,2 +1,6 @@
 <?php
 $string['pluginname'] = 'Bath Grades Transfer';
+$string['samis_assessment'] = '';
+$string['samis_assessment_help'] = 'Help text.......';
+$string['transfer_time_start'] = 'Transfer grades from';
+$string['transfer_time_start_help'] = 'Help text.....';
