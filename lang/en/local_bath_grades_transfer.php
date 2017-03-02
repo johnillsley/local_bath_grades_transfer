ce1a0c7068622aefc54c5c6064276414d21c34bb
Hittesh Ahuja <ha386@bath.ac.uk>
2017-03-02 11:19:55 +0000

    Added settings lang strings

--- lang/en/local_bath_grades_transfer.php
+++ lang/en/local_bath_grades_transfer.php
@@ -7,3 +7,7 @@ $string['samis_api_url_desc'] = 'SAMIS API URI to connect to ';
 $string['samis_api_key_desc'] = 'SAMIS API key goes here';
 $string['mod_choices'] = 'Enable in the following modules';
 $string['mod_choices_desc'] = 'Enable in the following modules';
+$string['transfer_time_start_help'] = 'Time to start the transfer';
+$string['transfer_time_start'] = 'Transfer from';
+$string['samis_assessment'] ='SAMIS Assessment to link to';
+$string['samis_assessment_help'] = 'Help text';
