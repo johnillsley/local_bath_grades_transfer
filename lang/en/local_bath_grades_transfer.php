5dbe42eb508e2e3f4f5c7d38464abb7e6d4265fc
Hittesh Ahuja <ha386@bath.ac.uk>
2017-03-02 11:14:38 +0000

    Added lang strings

---
+++ lang/en/local_bath_grades_transfer.php
@@ -0,0 +1,9 @@
+<?php
+$string['pluginname'] = 'Bath Grades Transfer';
+$string['samis_api_heading'] = 'SAMIS API Settings';
+$string['samis_api_key'] = 'SAMIS API Key';
+$string['samis_api_url'] = 'SAMIS API URI';
+$string['samis_api_url_desc'] = 'SAMIS API URI to connect to ';
+$string['samis_api_key_desc'] = 'SAMIS API key goes here';
+$string['mod_choices'] = 'Enable in the following modules';
+$string['mod_choices_desc'] = 'Enable in the following modules';
