534c17317083269b53f839e59579bc6e2a3ea5c3
Hittesh Ahuja <ha386@bath.ac.uk>
2017-03-16 14:11:15 +0000

    Added new event strings

--- lang/en/local_bath_grades_transfer.php
+++ lang/en/local_bath_grades_transfer.php
@@ -9,8 +9,13 @@ $string['mod_choices'] = 'Enable in the following modules';
 $string['mod_choices_desc'] = 'Enable in the following modules';
 $string['bath_grade_transfer_time_start_help'] = 'Time to start the transfer';
 $string['bath_grade_transfer_time_start'] = 'Transfer from';
-$string['bath_grade_transfer_samis_assessment'] ='SAMIS Assessment to link to';
+$string['bath_grade_transfer_samis_assessment'] = 'SAMIS Assessment to link to';
 $string['bath_grade_transfer_samis_assessment_help'] = 'Help text';
 $string['bath_grade_transfer_not_samis_default_mapping'] = 'Course does not seems to have a valid SAMIS Mapping.Please ensure course has a default mapping added via the SAMIS block';
 $string['bath_grade_transfer_settings_locked'] = 'Settings have been locked as atleast 1 grade has been transferred';
 $string['bath_grade_transfer_grade_not_hundred'] = 'Grades should be exactly out of 100 for Grades Transfer to work';
+
+
+/////EVENTS ////////
+$string['grade_transferred'] = 'Grade Transferred';
+$string['samis_mapping_warning'] = 'Please ensure you have contacted SAMIS before setting up mappings';
