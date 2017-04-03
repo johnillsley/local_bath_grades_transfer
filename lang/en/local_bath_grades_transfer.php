50669854c93854c0c85e3442a1e46a3630bc53c3
Hittesh Ahuja <ha386@bath.ac.uk>
2017-04-03 16:36:47 +0100

    Added more strings

--- lang/en/local_bath_grades_transfer.php
+++ lang/en/local_bath_grades_transfer.php
@@ -1,7 +1,11 @@
 <?php
 $string['pluginname'] = 'Bath Grades Transfer';
 $string['samis_api_heading'] = 'SAMIS API Settings';
-$string['samis_api_key'] = 'SAMIS API Key';
+$string['samis_api_key'] = 'SAMIS API Hash';
+$string['samis_api_user'] = 'SAMIS API User';
+$string['samis_api_user_desc'] = 'SAMIS API User';
+$string['samis_api_password'] = 'SAMIS API Password';
+$string['samis_api_password_desc'] = 'SAMIS API Password';
 $string['samis_api_url'] = 'SAMIS API URI';
 $string['samis_api_url_desc'] = 'SAMIS API URI to connect to ';
 $string['samis_api_key_desc'] = 'SAMIS API key goes here';
@@ -14,7 +18,6 @@ $string['bath_grade_transfer_samis_assessment_help'] = 'Help text';
 $string['bath_grade_transfer_not_samis_default_mapping'] = 'Course does not seems to have a valid SAMIS Mapping.Please ensure course has a default mapping added via the SAMIS block';
 $string['bath_grade_transfer_settings_locked'] = 'Settings have been locked as atleast 1 grade has been transferred';
 $string['bath_grade_transfer_grade_not_hundred'] = 'Grades should be exactly out of 100 for Grades Transfer to work';
-
 
 /////EVENTS ////////
 $string['grade_transferred'] = 'Grade Transferred';
