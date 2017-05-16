e6d1b67d6626a4819f61d474f058e6271f223a50
Hittesh Ahuja <ha386@bath.ac.uk>
2017-05-16 09:54:35 +0100

    Added new log class

---
+++ classes/transfer.log.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15/03/2017
+ * Time: 16:22
+ */
+class local_bath_grades_log
+{
+    /**
+     * @var string
+     */
+    private static $table  ='local_bath_grades_log';
+
+    /**
+     *
+     */
+    public static function get_logs(){
+        global $DB;
+
+    }
+
+    /**
+     *
+     */
+    public static function write_log(){
+
+    }
+
+
+}
