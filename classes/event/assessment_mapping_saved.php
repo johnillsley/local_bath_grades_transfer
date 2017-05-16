57fd8dfe892e7336d1dfde27d27cdcbbb4a5e574
Hittesh Ahuja <ha386@bath.ac.uk>
2017-05-16 09:54:50 +0100

    Added comments

--- classes/event/assessment_mapping_saved.php
+++ classes/event/assessment_mapping_saved.php
@@ -1,14 +1,18 @@
 <?php
-namespace block_bath_samis_grades_transfer\event;
+namespace local_bath_grades_transfer\event;
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 09/02/2017
- * Time: 14:57
+ * Class for event to be triggered when SAMIS assessment mapping attributes have been changed.
+ *
+ *
+ * @package    core
+ * @since      Moodle 3.1
+ * @copyright  2017 onwards University of Bath
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class assessment_mapping_saved extends \core\event\base {
+class assessment_mapping_saved extends \core\event\base
+{
     /**
      * Init method.
      *
@@ -16,8 +20,11 @@ class assessment_mapping_saved extends \core\event\base {
      */
     protected function init() {
         $this->data['crud'] = 'u';
-        $this->data['edulevel'] = self::LEVE;
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['action'] = 'assessment_mapping_updated';
+        $this->data['target'] = 'local_bath_grades_transfer_assessment';
     }
+
     /**
      * Return localised event name.
      *
@@ -26,13 +33,15 @@ class assessment_mapping_saved extends \core\event\base {
     public static function get_name() {
         return get_string('assessmentmapped', 'local_bath_grades_transfer');
     }
+
     /**
      * Returns description of what happened.
      *
      * @return string
      */
     public function get_description() {
-        return "The user with id '$this->userid' modified the assessment in SAMIS '$this->courseid'.";
+        return "blah!";
+        return "The user with id" . $this->userid . " has set SAMIS Assessment to " . $this->other['assessment_name'];
     }
 
 }
