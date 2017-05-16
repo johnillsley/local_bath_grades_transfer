892293783c437e1312617614d1ee7ea90884a06e
Hittesh Ahuja <ha386@bath.ac.uk>
2017-05-16 09:55:05 +0100

    Added new lang string

--- lang/en/local_bath_grades_transfer.php
+++ lang/en/local_bath_grades_transfer.php
@@ -12,14 +12,14 @@ $string['samis_api_key_desc'] = 'SAMIS API key goes here';
 $string['mod_choices'] = 'Enable in the following modules';
 $string['mod_choices_desc'] = 'Enable in the following modules';
 $string['bath_grade_transfer_time_start_help'] = 'Time to start the transfer';
-$string['samis_mapping_warning'] = 'Please ensure you have contacted SAMIS before setting up mappings';
+$string['samis_mapping_warning'] = 'Please ensure you have contacted the SAMIS team before setting up these assessments';
 $string['bath_grade_transfer_time_start'] = 'Transfer from';
 $string['bath_grade_transfer_samis_assessment_id'] = 'SAMIS Assessment to link to';
 $string['bath_grade_transfer_samis_assessment_id_help'] = $string['samis_mapping_warning'];
 $string['bath_grade_transfer_not_samis_default_mapping'] = 'Course does not seems to have a valid SAMIS Mapping.Please ensure course has a default mapping added via the SAMIS block';
 $string['bath_grade_transfer_settings_locked'] = 'Settings have been locked as atleast 1 grade has been transferred';
 $string['bath_grade_transfer_grade_not_hundred'] = 'Grades should be exactly out of 100 for Grades Transfer to work';
-
+$string['settings_locked'] = '!!! Settings are now locked !!!';
 /////EVENTS ////////
 $string['grade_transferred'] = 'Grade Transferred';
 $string['assessmentmapped'] = ' SAMIS Assessment Mapped to Moodle Activity';
