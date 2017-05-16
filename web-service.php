c1438a0d04fb60c89dd08239053626d8078d54db
Hittesh Ahuja <ha386@bath.ac.uk>
2017-05-16 09:57:33 +0100

    dummy xml web service server

--- web-service.php
+++ web-service.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 //Example : <moodle>/block/bath_samis_grades_transfer/web-service.php?method=GetSamisAssessmentDetailsForCourse&samis_code=ED00000
 //mod_code
 //mav_occur
@@ -6,72 +21,100 @@
 //psl_code
 $method = $_GET['method'];
 //$samis_code = $_GET['samis_code'];
-
-switch($method){
-    case 'GetSamisAssessmentDetailsForCourse_OLD':
-         $data =  array('assessments'=> array('assessment'=>array(
-            'module'=>'ED00000',
-            'occurrence'=> 'A',
-            'year'=>'2016/7',
-            'period'=>'AY',
-            'assess_item'=>'01',
-             'student' => '029005235/1'
-        )));
-        echo json_output($data);
-        break;
+switch ($method) {
     case 'GetSamisAssessmentDetailsForCourse':
-        $data =  array('assessments'=> array('assessment'=>array(
-            'module'=>'ED00000',
-            'occurrence'=> 'A',
-            'year'=>'2016/7',
-            'period'=>'AY',
-            'map_code'=>'ED00000A',
-            'mab_seq'=>'01',
-            'mab_name'=> 'CourseWork (CW1) '
-
-        ),
-            array(
-                'module'=>'ED00000',
-                'occurrence'=> 'A',
-                'year'=>'2016/7',
-                'period'=>'AY',
-                'map_code'=>'ED00000A',
-                'mab_seq'=>'02',
-                'mab_name'=> 'CourseWork (CW2) '
-            )));
-        echo json_output($data);
+        $data = <<<XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<records>
+    <assessments>
+        <assessment>
+            <module>CH40236</module>
+            <occurrence>A</occurrence>
+            <year>2016/7</year>
+            <period>AY</period>
+            <map_code>CH40236A</map_code>
+            <mab_seq>01</mab_seq>
+            <mab_name>CH40236 Exam</mab_name>
+        </assessment>
+    </assessments>
+</records>
+XML;
+        break;
+    case 'GetSamisGradesStructure':
+         $data = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<records>
+   <assessments>
+          
+      <assessment>
+                 
+         <module>CH40236</module>
+                 
+         <occurrence>A</occurrence>
+                 
+         <year>2016/7</year>
+                 
+         <period>AY</period>
+                 
+         <assess_item>01</assess_item>
+                 
+         <student>029005235/1</student>
+                 
+         <mark>33</mark>
+             
+      </assessment>
+          
+      <assessment>
+                 
+         <module>CH40236</module>
+                 
+         <occurrence>A</occurrence>
+                 
+         <year>2016/7</year>
+                 
+         <period>AY</period>
+                 
+         <assess_item>01</assess_item>
+                 
+         <student>029005237/1</student>
+                 
+         <mark>44</mark>
+             
+      </assessment>
+          
+      <assessment>
+                 
+         <module>CH40236</module>
+                 
+         <occurrence>A</occurrence>
+                 
+         <year>2016/7</year>
+                 
+         <period>AY</period>
+                 
+         <assess_item>01</assess_item>
+                 
+         <student>029125235/1</student>
+                 
+         <mark>80</mark>
+             
+      </assessment>
+   </assessments>
+</records>
+XML;
+        break;
+    case 'GetSPRFromBUCSID':
+        $data = <<<XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<records>
+    <spr_code>029005235/1</spr_code>
+</records>
+XML;
         break;
 }
-function json_output($data){
+echo $data;
+function json_output($data) {
     header('Content-type: application/json');
     return json_encode($data);
 }
-function xml_output($data){
-    header('Content-type: text/xml');
-    $xml_user_info = new SimpleXMLElement("<?xml version=\"1.0\"?><records></records>");
-    //function call to convert array to xml
-    array_to_xml($data,$xml_user_info);
-    //saving generated xml file
-    $xml_file = $xml_user_info->asXML('users.xml');
-}
-
-//function defination to convert array to xml
-function array_to_xml($array, &$xml_user_info) {
-    foreach($array as $key => $value) {
-        if(is_array($value)) {
-            if(!is_numeric($key)){
-                $subnode = $xml_user_info->addChild("$key");
-                array_to_xml($value, $subnode);
-            }else{
-                $subnode = $xml_user_info->addChild("item$key");
-                array_to_xml($value, $subnode);
-            }
-        }else {
-            $xml_user_info->addChild("$key",htmlspecialchars("$value"));
-        }
-    }
-}
-
-//creating object of SimpleXMLElement
-
 
