7dc9c3c03176bc902738840475f0456b9c269e5d
Hittesh Ahuja <ha386@bath.ac.uk>
2017-05-25 13:35:41 +0100

    Added transfer log methods

--- classes/transfer.log.class.php
+++ classes/transfer.log.class.php
@@ -9,6 +9,38 @@
 class local_bath_grades_log
 {
     /**
+     * @var
+     */
+    public $id;
+    /**
+     * @var
+     */
+    public $coursemoduleid;
+    /**
+     * @var
+     */
+    public $userid;
+    /**
+     * @var
+     */
+    public $gradetransfermappingid;
+    /**
+     * @var
+     */
+    public $assessment_lookup_id;
+    /**
+     * @var
+     */
+    public $timetransferred;
+    /**
+     * @var
+     */
+    public $outcomeid;
+    /**
+     * @var
+     */
+    public $grade_transfer_error_id;
+    /**
      * @var string
      */
     private static $table  ='local_bath_grades_log';
@@ -22,9 +54,23 @@ class local_bath_grades_log
     }
 
     /**
+     * @param $id
+     */
+    public static function get_log_by_id($id){
+
+    }
+
+    /**
      *
      */
     public static function write_log(){
+
+    }
+
+    /**
+     *
+     */
+    public function save(){
 
     }
 
