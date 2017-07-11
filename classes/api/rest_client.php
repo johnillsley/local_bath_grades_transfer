69c1fec566254c692acd69da4409e8c22ef61676
Hittesh Ahuja <ha386@bath.ac.uk>
2017-07-11 13:20:43 +0100

    No need for constant -Added async get and post methods

--- classes/api/rest_client.php
+++ classes/api/rest_client.php
@@ -1,9 +1,12 @@
 <?php
 global $CFG;
-require_once $CFG->dirroot.'/local/bath_grades_transfer/vendor/autoload.php';
+require_once $CFG->dirroot . '/local/bath_grades_transfer/vendor/autoload.php';
 use \GuzzleHttp\Client;
+use GuzzleHttp\Promise;
 use \GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
 use \GuzzleHttp\Exception\RequestException;
+
 /**
  * Created by PhpStorm.
  * User: Administrator
@@ -12,77 +15,144 @@ use \GuzzleHttp\Exception\RequestException;
  */
 class local_bath_grades_transfer_rest_client
 {
+    /**
+     * @var
+     */
     private $response;
+    /**
+     * @var
+     */
     public $promise;
+    /**
+     * @var mixed
+     */
     private $username;
+    /**
+     * @var mixed
+     */
     private $password;
+    /**
+     * @var
+     */
     private $is_connected;
+    /**
+     *
+     */
 
+    /**
+     * local_bath_grades_transfer_rest_client constructor.
+     */
     public function __construct() {
         $api_url = get_config('local_bath_grades_transfer', 'samis_api_url');
         $this->username = get_config('local_bath_grades_transfer', 'samis_api_user');
         $this->password = get_config('local_bath_grades_transfer', 'samis_api_password');
         $this->client = new Client([
-            'base_uri'=>$api_url
+                'base_uri' => $api_url
             ]
         );
-
     }
-    private  function test_connection(){
-        try{
+
+    /**
+     *
+     */
+    private function test_connection() {
+        try {
             $this->client->request('GET', '/');
-        }
-        catch(\GuzzleHttp\Exception\ClientException $e){
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
             echo $e->getMessage();
             echo $e->getCode();
         }
     }
-    public function is_connected(){
+
+    /**
+     * @return mixed
+     */
+    public function is_connected() {
         return $this->is_connected;
     }
-    private function construct_body(array $pieces){
+
+    /**
+     * @param array $pieces
+     * @return string
+     */
+    private function construct_body(array $pieces) {
         $glue = '/';
         $body_raw = '';
         $lastElement = end($pieces);
-        foreach($pieces as $key => $value){
-            if($value == $lastElement){
-                $body_raw .= $key.$glue.$value;
-            }
-            else{
-                $body_raw .= $key.$glue.$value.$glue;
+        foreach ($pieces as $key => $value) {
+            if ($value == $lastElement) {
+                $body_raw .= $key . $glue . $value;
+            } else {
+                $body_raw .= $key . $glue . $value . $glue;
             }
         }
         return $body_raw;
     }
 
-    public function call_samis($method,$data){
-         //Convert the data into SAMIS format
-        $data_raw = $this->construct_body($data);
-         try{
-            $this->response = $this->client->request('GET','https://www.bath.ac.uk/samis-dev/urd/sits.urd/run/SIW_RWS/'.$method.'/'.$data_raw,[
-                'debug' => false,
-                'auth' => [$this->username, $this->password],
-                'headers' => [
-                    //'Content-Type' => 'text/xml', //dont need header for now as output is always JSON
-                    'Cache-Control' => 'no-cache',
-                ],
-            ]);
-             return $this->response->getBody()->getContents();
+    /**
+     * @param $method
+     * @param $data
+     * @param string $verb
+     * @return mixed
+     * @throws Exception
+     */
+    public function call_samis($method, $data, $verb = 'GET') {
+        try {
+            $data_raw = $this->construct_body($data);
+            if ($verb == 'POST') {
+                //post changes
 
-        }
-        catch(\GuzzleHttp\Exception\ClientException $e){
+                $this->promise = $this->client->postAsync( $method . '/' . $data_raw, [
+                    'debug' => false,
+                    'auth' => [$this->username, $this->password],
+                    'headers' => [
+                        'Content-Type' => 'text/xml',
+                        'Cache-Control' => 'no-cache',
+                    ],
+                    'body' => $data['body']
+                ]);
+            } else {
+                $this->promise = $this->client->getAsync( $method . '/' . $data_raw, [
+                    'debug' => false,
+                    'auth' => [$this->username, $this->password],
+                    'headers' => [
+                        'Content-Type' => 'text/xml',
+                        'Cache-Control' => 'no-cache',
+                    ]
+                ]);
+            }
+            echo "\n\n++++++++++++REQUEST SENT !! WAITING FOR PROMISE TO COME BACK++++++++++++\n\n";
+            return $this->promise->then(
+            //success Callback
+                function (ResponseInterface $res) {
+                    echo "PROMISE FULFILLED !";
+                    return $res->getBody()->getContents();
+                },
+                //error handling
+                function (RequestException $e) {
+                    if ($e->getCode() == 400) {
+                        //Bad Request.
+                        throw  new \Exception($e->getMessage());
+                    }
+
+                    if ($e->getCode() == 404) {
+                        throw  new \Exception("Cant connect to SAMIS");
+                    }
+                }
+            )->wait();
+
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
             echo "Throwing Client Exception Exception #1";
-            if($e->getCode() == 400){
+            if ($e->getCode() == 400) {
                 //Bad Request.
-                 throw  new \Exception($e->getMessage());
+                throw  new \Exception($e->getMessage());
             }
 
-            if($e->getCode() == 404){
+            if ($e->getCode() == 404) {
                 throw  new \Exception("Cant connect to SAMIS");
             }
 
-        }
-        catch(\GuzzleHttp\Exception\ServerException $e){
+        } catch (\GuzzleHttp\Exception\ServerException $e) {
             echo "Throwing Server Exception Exception #1";
             //Treat it as we did not get any data
             echo $e->getMessage();
