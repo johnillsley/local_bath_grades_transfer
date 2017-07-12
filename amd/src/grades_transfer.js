1543e6ed71f9155b136b9b8298887a363b8cf852
Hittesh Ahuja <ha386@bath.ac.uk>
2017-07-12 14:52:32 +0100

    Added msg when checked and unchecked

--- amd/src/grades_transfer.js
+++ amd/src/grades_transfer.js
@@ -1,11 +1,17 @@
 define(['jquery'], function ($) {
     return {
         init: function () {
+
            //Show msg
             $( "#id_unlock_assessment" ).on( "click",function(){
-                //Show a friendly warning message
-                $(this).after("<div class=\"alert alert-warning alert-dismissable\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Success!</strong> Indicates a successful or positive action.</div>");
-
+                if($( this ).prop( "checked" )){
+                    //Show a friendly warning message
+                    $('#unlock-msg').show();
+                }
+                else{
+                    //Show a friendly warning message
+                    $('#unlock-msg').hide();
+                }
             });
         }
     };
