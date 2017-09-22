a91d544e685499688872a9ce8e658124b6ad72b4
Hittesh Ahuja <ha386@bath.ac.uk>
2017-09-22 17:17:56 +0100

    Added new events

---
+++ classes/event/assessment_mapping_unlocked.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_bath_grades_transfer\event;
+defined('MOODLE_INTERNAL') || die();
+/**
+ * Class for event to be triggered when user unlocks an assessment mapping
+ *
+ *
+ * @package    core
+ * @since      Moodle 3.1
+ * @copyright  2017 onwards University of Bath
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class assessment_mapping_unlocked extends \core\event\base
+{
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'u';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        //$this->data['action'] = 'assessment_mapping_unlocked';
+        //$this->data['target'] = 'local_bath_grades_transfer_assessment';
+        //$this->data['objecttable'] = 'local_bath_grades_mapping';
+    }
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return 'Assessment Mapping Unlocked';
+
+    }
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "Assessment mapping has been unlocked ";
+        //return "The user with id" . $this->userid . " has set SAMIS Assessment to " . $this->other['assessment_name'];
+    }
+}
