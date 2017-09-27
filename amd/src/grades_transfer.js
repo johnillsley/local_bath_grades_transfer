48332d59ee667a14d22fc6b03e618e4b3b3f98dd
Hittesh Ahuja <ha386@bath.ac.uk>
2017-09-27 09:36:58 +0100

    Code reformat

--- amd/src/grades_transfer.js
+++ amd/src/grades_transfer.js
@@ -1,14 +1,13 @@
 define(['jquery'], function ($) {
     return {
         init: function () {
-
-           //Show msg
-            $( "#id_unlock_assessment" ).on( "click",function(){
-                if($( this ).prop( "checked" )){
+            //Show msg
+            $("#id_unlock_assessment").on("click", function () {
+                if ($(this).prop("checked")) {
                     //Show a friendly warning message
                     $('#unlock-msg').show();
                 }
-                else{
+                else {
                     //Show a friendly warning message
                     $('#unlock-msg').hide();
                 }
