0a224de01fff1d05bd2d5e1c86e611997de33048
Hittesh Ahuja <ha386@bath.ac.uk>
2017-10-04 11:42:26 +0100

    - Target checkbox - Added animation for "Fetch new assessments" msg

--- amd/src/grades_transfer.js
+++ amd/src/grades_transfer.js
@@ -1,8 +1,10 @@
 define(['jquery'], function ($) {
     return {
         init: function () {
+            //Fetched any new assessments msg
+            $("#fetched_new_assessments_notif").prependTo('#region-main').fadeOut(3000);
             //Show msg
-            $("#id_unlock_assessment").on("click", function () {
+            $("input[name='bath_grade_transfer_samis_unlock_assessment']").on("click", function () {
                 if ($(this).prop("checked")) {
                     //Show a friendly warning message
                     $('#unlock-msg').show();
