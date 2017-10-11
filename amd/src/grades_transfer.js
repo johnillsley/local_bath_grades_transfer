4744c28deed46c1c98513e902241f2f5d3105dd3
Hittesh Ahuja <ha386@bath.ac.uk>
2017-10-11 14:05:10 +0100

    Added animation

--- amd/src/grades_transfer.js
+++ amd/src/grades_transfer.js
@@ -2,7 +2,11 @@ define(['jquery'], function ($) {
     return {
         init: function () {
             //Fetched any new assessments msg
-            $("#fetched_new_assessments_notif").prependTo('#region-main').fadeOut(3000);
+            $("#fetched_new_assessments_notif").prependTo('#region-main').animate(
+                {
+                    top: '350px'
+                }, 'slow'
+            ).fadeOut(5000);
             //Show msg
             $("input[name='bath_grade_transfer_samis_unlock_assessment']").on("click", function () {
                 if ($(this).prop("checked")) {
