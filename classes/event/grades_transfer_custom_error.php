fc43608ac2bedcafceff244016a5dbdc58d20960
Hittesh Ahuja <ha386@bath.ac.uk>
2017-10-17 11:40:17 +0100

    Added new event class for GT Custom Error

---
+++ classes/event/grades_transfer_custom_error.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_bath_grades_transfer\event;
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class for event to be triggered when a Moodle Module has been deleted.
+ *
+ *
+ * @package    core
+ * @since      Moodle 3.1
+ * @copyright  2017 onwards University of Bath
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class grades_transfer_custom_error extends \core\event\base
+{
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'u';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return 'Grades Transfer Custom Error';
+
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "ERROR IN GRADE TRANSFER :  " . $this->other['message'];
+    }
+}
