8bcf44ec7674a617fb8f91d8387330f32db2b5ed
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2017-10-20 17:09:26 +0100

    Added ajax file to test connection to SAMIS

---
+++ test_connection.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define('AJAX_SCRIPT', true);
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->dirroot . '/local/bath_grades_transfer/classes/api/rest_client.php');
+$testconn = required_param('system', PARAM_RAW);
+//Test connection SAMIS
+$restclient = new local_bath_grades_transfer_rest_client();
+$restclient->test_connection();
+ $status = new stdClass();
+if ($restclient->isconnected) {
+    $status->connected = true;
+
+} else {
+    $status->connected = false;
+}
+echo json_encode($status);
