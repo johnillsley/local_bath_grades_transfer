6d413594982a2c235d5ba65c9fe4b6fa3cb35849
John Illsley <jsi22@bath.ac.uk>
2017-11-28 15:21:48 +0000

    Update to SQL to remove duplicates from student list

--- lib.php
+++ lib.php
@@ -963,7 +963,7 @@ WHERE userid = ? AND gradetransfermappingid =
             AND sm.default_map = 1
         JOIN {sits_mappings_enrols} me ON me.map_id = sm.id
         JOIN {user_enrolments} ue ON ue.id = me.u_enrol_id -- PROBLEM WITH user_enrolments BEING REMOVED!!!
-        JOIN {user} u ON u.id = ue.userid
+        JOIN {user} u ON u.id = ue.userid AND ra.id = me.ra_id
         JOIN {role_assignments} ra 
             ON ra.userid = u.id
             AND contextid = ".$context->id."
