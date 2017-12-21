ebfc573a95d0feb4d2413b5df26bf8e23cfb1599
Hittesh Ahuja <ha386@bath.ac.uk>
2017-12-21 10:40:03 +0000

    - Code for literal outcomes applied

--- classes/log.php
+++ classes/log.php
@@ -61,15 +61,29 @@ class local_bath_grades_transfer_log
     /**
      *
      */
-    public static function get_logs($userid,$mappingid,$limit = null,$literal_outcomes = false) {
+    public static function get_logs($userid,$mappingid,$limit = null,$literaloutcomes = false) {
         global $DB;
         $logs = array();
-        $sql = "SELECT FROM_UNIXTIME(l.timetransferred) as 'timetransferred',o.outcome,o.id,l.gradetransferred FROM {local_bath_grades_log} l";
-        $join = " JOIN {local_bath_grades_outcome} o ON o.id = l.outcomeid";
+        $fields = array();
+        $fields[] = 'FROM_UNIXTIME(l.timetransferred) as \'timetransferred\'';
+        $fields[] = 'l.gradetransferred';
+        $sql = 'SELECT ';
+        $sql .= implode(',',$fields);
+        $table = '{local_bath_grades_log} l ';
 
-        if($literal_outcomes){
+        if($literaloutcomes){
+            $otherfields[] = ',o.outcome';
+            $otherfields[] = 'o.id';
+            $sql .= implode(',',$otherfields);
+            $sql .= ' FROM '.$table;
+            $join = " JOIN {local_bath_grades_outcome} o ON o.id = l.outcomeid";
+
             $sql .= $join;
         }
+        else{
+            $sql .= ' FROM '.$table;
+        }
+
         $sql .= " WHERE l.userid = :userid AND l.gradetransfermappingid = :mappingid ORDER BY l.timetransferred DESC";
         $rs = $DB->get_recordset_sql($sql,array('userid'=>$userid,'mappingid'=>$mappingid),0,$limit);
         if($rs->valid()){
