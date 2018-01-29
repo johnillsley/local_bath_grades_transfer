e078c2984abd0906ea51c5277d1b1b08e81be793
Hittesh Ahuja <ha386@bath.ac.uk>
2018-01-29 15:14:43 +0000

    removed space

--- classes/error.php
+++ classes/error.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 defined('MOODLE_INTERNAL') || die();
-
 class local_bath_grades_transfer_error
 {
     public $id;
